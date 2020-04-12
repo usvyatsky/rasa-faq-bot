5257b9c58f462fa2a0858fa8bebe65284977059f
Ilya Usvyatsky <usvyatsky@gmail.com>
2020-04-12 11:03:05 -0400

    New FAQ, actions, basic and endpoints

--- actions.py
+++ actions.py
@@ -15,14 +15,18 @@ from rasa_sdk.executor import CollectingDispatcher
 import numpy as np
 
 
+bertHost = 'bert'
+
+
 class ActionGetFAQAnswer(Action):
 
     def __init__(self):
         super(ActionGetFAQAnswer, self).__init__()
-        self.bc = BertClient()
-        self.faq_data = json.load(open("./data/nlu/faq.json", "rt", encoding="utf-8"))
-        self.standard_questions_encoder = np.load("./data/standard_questions.npy")
-        self.standard_questions_encoder_len = np.load("./data/standard_questions_len.npy")
+        self.bc = BertClient(bertHost)
+        encode_standard_question(self.bc)
+        self.faq_data = json.load(open("./faq.json", "rt", encoding="utf-8"))
+        self.standard_questions_encoder = np.load("./standard_questions.npy")
+        self.standard_questions_encoder_len = np.load("./standard_questions_len.npy")
         print(self.standard_questions_encoder.shape)
 
     def get_most_similar_standard_question_id(self, query_question):
@@ -43,7 +47,6 @@ class ActionGetFAQAnswer(Action):
         if float(score) > 0.93:
             response = self.faq_data[most_similar_id]['a']
             dispatcher.utter_message(response)
-            dispatcher.utter_message("Problem solved?")
         else:
             response = "Sorry, this question is beyond my ability..."
             dispatcher.utter_message(response)
@@ -51,17 +54,17 @@ class ActionGetFAQAnswer(Action):
         return []
 
 
-def encode_standard_question():
-    bc = BertClient()
-    data = json.load(open("./data/nlu/faq.json", "rt", encoding="utf-8"))
+def encode_standard_question(bc):
+    data = json.load(open("./faq.json", "rt", encoding="utf-8"))
     standard_questions = [each['q'] for each in data]
     print("Standard question size", len(standard_questions))
     print("Start to calculate encoder....")
     standard_questions_encoder = bc.encode(standard_questions)
-    np.save("./data/standard_questions", standard_questions_encoder)
+    np.save("./standard_questions", standard_questions_encoder)
     standard_questions_encoder_len = np.sqrt(np.sum(standard_questions_encoder * standard_questions_encoder, axis=1))
-    np.save("./data/standard_questions_len", standard_questions_encoder_len)
+    np.save("./standard_questions_len", standard_questions_encoder_len)
 
 
 if __name__ == '__main__':
-    encode_standard_question()
+    bc = BertClient(bertHost)
+    encode_standard_question(bc)
