4a7ba6ae86c5e6c901f331323fac4ecdb7f1a7c0
Ilya Usvyatsky <usvyatsky@gmail.com>
2020-04-22 00:35:27 -0400

    Added S3 bucket as a source for FAQ

--- actions.py
+++ actions.py
@@ -13,9 +13,13 @@ from bert_serving.client import BertClient
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import numpy as np
+import os
+import boto3
 
 
 bertHost = 'bert'
+BUCKET = 'BUCKET'
+FAQ = 'FAQ'
 
 
 class ActionGetFAQAnswer(Action):
@@ -23,48 +27,74 @@ class ActionGetFAQAnswer(Action):
     def __init__(self):
         super(ActionGetFAQAnswer, self).__init__()
         self.bc = BertClient(bertHost)
-        encode_standard_question(self.bc)
-        self.faq_data = json.load(open("./faq.json", "rt", encoding="utf-8"))
-        self.standard_questions_encoder = np.load("./standard_questions.npy")
-        self.standard_questions_encoder_len = np.load("./standard_questions_len.npy")
-        print(self.standard_questions_encoder.shape)
+        self.faq, self.encoder, self.encoder_len = encode_faq(self.bc)
+        print(self.encoder.shape)
 
-    def get_most_similar_standard_question_id(self, query_question):
+    def find_question(self, query_question):
         query_vector = self.bc.encode([query_question])[0]
-        score = np.sum((self.standard_questions_encoder * query_vector), axis=1) / (
-                self.standard_questions_encoder_len * (np.sum(query_vector * query_vector) ** 0.5))
+        score = np.sum((self.encoder * query_vector), axis=1) / (
+                self.encoder_len * (np.sum(query_vector * query_vector) ** 0.5))
         top_id = np.argsort(score)[::-1][0]
         return top_id, score[top_id]
 
     def name(self) -> Text:
         return "action_get_answer"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+                self,
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]
+            ) -> List[Dict[Text, Any]]:
         query = tracker.latest_message['text']
-        most_similar_id, score = self.get_most_similar_standard_question_id(query)
+        most_similar_id, score = self.find_question(query)
         if float(score) > 0.93:
-            response = self.faq_data[most_similar_id]['a']
+            response = self.faq[most_similar_id]['a']
             dispatcher.utter_message(response)
         else:
             response = "Sorry, this question is beyond my ability..."
             dispatcher.utter_message(response)
-            dispatcher.utter_message("Sorry, I can't answer your question. You can dial the manual serviece...")
         return []
 
 
-def encode_standard_question(bc):
-    data = json.load(open("./faq.json", "rt", encoding="utf-8"))
-    standard_questions = [each['q'] for each in data]
-    print("Standard question size", len(standard_questions))
-    print("Start to calculate encoder....")
-    standard_questions_encoder = bc.encode(standard_questions)
-    np.save("./standard_questions", standard_questions_encoder)
-    standard_questions_encoder_len = np.sqrt(np.sum(standard_questions_encoder * standard_questions_encoder, axis=1))
-    np.save("./standard_questions_len", standard_questions_encoder_len)
+def get_faq():
+    bucket = None
+    faq = 'faq.json'
+    if BUCKET in os.environ:
+        bucket = os.environ[BUCKET]
+    if FAQ in os.environ:
+        faq = os.environ[FAQ]
+    if bucket and faq:
+        s3client = boto3.client('s3')
+        try:
+            r = s3client.get_object(Bucket=bucket, Key=faq)
+            data = json.load(r['Body'])
+            with open(os.path.basename(faq), 'w') as f:
+                json.dump(data, f)
+            return data
+        except Exception as ex:
+            print(f'Cannot get FAQ from s3://{bucket}/{faq}: {ex}')
+    if not os.path.exists(faq):
+        faq = os.path.basename(faq)
+    if os.path.exists(faq):
+        with open(faq, "rt", encoding="utf-8") as f:
+            return json.load(f)
+    return None
+
+
+def encode_faq(bc):
+    faq = get_faq()
+    questions = [each['q'] for each in faq]
+    print(f'FAQ size {len(questions)}')
+    print('Calculating encoder')
+    encoder = bc.encode(questions)
+    np.save("./questions", encoder)
+    encoder_len = np.sqrt(np.sum(encoder * encoder, axis=1))
+    np.save("./questions_len", encoder_len)
+    print('FAQ encoded')
+    return faq, encoder, encoder_len
 
 
 if __name__ == '__main__':
     bc = BertClient(bertHost)
-    encode_standard_question(bc)
+    encode_faq(bc)
